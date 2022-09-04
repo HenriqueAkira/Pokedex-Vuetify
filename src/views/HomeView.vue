12d407f65a8bfc90b897cbb38b300e7d291725d3
HenriqueAkira <henrique.yasuda@hotmail.com>
2022-09-04 01:46:57 -0300

    vuetify added and grid started

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,42 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-container class="grey lighten-5" fluid>
+
+    <v-row>
+      <v-col v-for="pokemon in pokemonList" :key="pokemon.name" cols="12" md="6" lg="3" sm="6">
+
+        <v-card class="mb-10 text-center fill-height">
+            <v-img contain height="150" src="@/assets/logo.png"></v-img>
+            <v-divider></v-divider>
+            <v-card-title>
+              {{pokemon.name}}
+            </v-card-title>
+            <v-card-text>
+              {{pokemon.content}}
+            </v-card-text>
+        </v-card>
+
+      </v-col>
+    </v-row>
+
+  </v-container>
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
-export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
+
+
+<script>
+
+  export default {
+
+    data(){
+      return{
+        pokemonList: [{img:"@/assets/logo.png", name:"Pikachu", content:"Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quidem qui commodi ipsam modi molestiae nobis est, assumenda explicabo, dignissimos rerum incidunt quam beatae doloremque alias voluptates, nihil impedit placeat esse!"},
+        {img:"@/assets/logo.png", name:"Pikachu2", content:"Lorem ipsum doing elit. Quidem qui commodi ipsam modi molestiae nobis est, assumenda explicabo, dignissimos rerum incidunt quam beatae doloremque alias voluptates, nihil impedit placeat esse!"},
+        {img:"@/assets/logo.png", name:"Pikachu3", content:"sit, amet consectetur adipisicing elit. Quidem qui commodi ipsam modi molestiae nobis est, assumenda explicabo, dignissimos rerum incidunt quam beatae doloremque alias voluptates, nihil impedit placeat esse!"}]
+      }
+    },
+
+    components: {
+    },
   }
-}
 </script>
