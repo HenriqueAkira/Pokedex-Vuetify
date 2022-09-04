dce018945bd764172c72eb9ae1f947740111b420
HenriqueAkira <henrique.yasuda@hotmail.com>
2022-09-04 16:02:32 -0300

    Working with pokemon API

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -5,13 +5,17 @@
       <v-col v-for="pokemon in pokemonList" :key="pokemon.name" cols="12" md="6" lg="3" sm="6">
 
         <v-card class="mb-10 text-center fill-height">
-            <v-img contain height="150" src="@/assets/logo.png"></v-img>
+            <v-img contain height="150" :src="pokemon.sprite"></v-img>
             <v-divider></v-divider>
             <v-card-title>
               {{pokemon.name}}
             </v-card-title>
             <v-card-text>
-              {{pokemon.content}}
+              <v-row> 
+                <v-col v-for="type in pokemon.types" :key="type.type.name">
+                  {{type.type.name}}
+                </v-col>
+              </v-row>
             </v-card-text>
         </v-card>
 
@@ -51,7 +55,10 @@
         .then(data => data.results.map(pokemon =>{
           fetch(pokemon.url).then(response => response.json())
             .then(detailedData => {
-              console.log(detailedData);
+              pokemon.types = detailedData.types
+              pokemon.sprite = detailedData.sprites.front_default
+              this.pokemonList.push(pokemon)
+              console.log(pokemon);
             })
       }))
     }
