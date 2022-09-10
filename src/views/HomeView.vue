d4f50197a3a9e6e21cc2c9743b05a1273f7f0dff
HenriqueAkira <henrique.yasuda@hotmail.com>
2022-09-10 01:38:11 -0300

    Starting homepage

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,12 @@
 <template>
-  <div></div>
+  <div >
+    <div class="intro-img"></div>
+    <div class="intro-text">
+      <h1>Teste</h1>
+      <p>teste</p>
+    </div>
+  </div>
+  
 </template>
 
 <script>
@@ -9,5 +16,32 @@ export default {
 </script>
 
 <style>
+
+.intro-img{
+  height: 45em;
+  background-image: url("@/assets/background.gif");
+  background-repeat: no-repeat;
+  background-position: center;
+  background-size: cover;
+  filter: blur(4px);
+}
+
+.intro-text {
+  background-color: rgb(255, 255, 255); 
+  background-color: rgb(255, 255, 255);
+  color: rgb(219, 79, 79);
+  font-weight: bold;
+  border: 3px solid #f1f1f1;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  z-index: 2;
+  width: 80%;
+  padding: 20px;
+  text-align: center;
+}
+
+
 
 </style>
