86226cee14bef8cd622ba7c3d76165b4a2cce91a
HenriqueAkira <henrique.yasuda@hotmail.com>
2022-09-12 14:38:27 -0300

    parallax section finished

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -9,57 +9,74 @@
     </div>
 
     <v-parallax dark height="900" class="parallax" src="@/assets/grass-background.jpg">
-      <div class="pokedex-parallax mx-auto">
-          <v-carousel
-          cycle
-          height="600"
-          hide-delimiter-background
-          show-arrows-on-hover
-        >
-          <v-carousel-item
-            v-for="(pokemon, i) in pokemonsBanner"
-            :key="i"
-          >
-            <v-sheet
-              height="100%"
-            >
-              <v-row
-                class="fill-height"
-                align="center"
-              >
-                <v-col cols="6">
-                  <v-img :src="pokemon.sprite" width="400" height="auto" contain></v-img>
-                </v-col>
+      <v-row align="center" class="mt-10">
+        <v-col class="text-center" cols="12" md="6">
+          <h1><v-icon color="red" x-large>mdi-pokeball</v-icon></h1>
+          <p class="parallax-title text-uppercase">
+            The <span class="red--text font-weight-bold text-uppercase">Pokédex section</span> has a wealth of information on all the Pokémon creatures from the entire game series. 
+          </p>
+          <p class="parallax-text ">
+            On the main list pages you can see the various stats of each Pokémon. See a detailed page with Pokédex data,
+             descriptions, sprites, evolutions and stats!
+          </p>
+        </v-col>
 
-                <v-col cols="4" class="pa-6">
-                  
-                  <v-row class="pa-3">
-                    <div class="icon icon-banner pa-1 mr-4 mb-4" :class="`${type.type.name}`" v-for="type in pokemon.types" :key="type.type.name">
-                      <v-img contain :src="require(`@/assets/${type.type.name}.svg`)"></v-img>
-                    </div>
+        <v-col cols="12" md="6" align="center">
+          <div class="pokedex-parallax ">
+              <v-carousel
+              height="600"
+              hide-delimiter-background
+              show-arrows-on-hover
+            >
+              <v-carousel-item
+                v-for="(pokemon, i) in pokemonsBanner"
+                :key="i"
+              >
+                <v-sheet
+                color="rgba(220,220,220,0.8)"
+                  height="90%"
+                >
+                  <v-row
+                    id="carousel-item"
+                    class="fill-height"
+                    align="center"
+                  >
+                    <v-col cols="6">
+                      <v-img :src="pokemon.sprite" width="400" height="auto" contain></v-img>
+                    </v-col>
+
+                    <v-col cols="5" >
+                      
+                      <v-row class="pa-3">
+                        <div class="icon icon-banner pa-1 mr-4 mb-4" :class="`${type.type.name}`" v-for="type in pokemon.types" :key="type.type.name">
+                          <v-img contain :src="require(`@/assets/${type.type.name}.svg`)"></v-img>
+                        </div>
+                      </v-row>
+                        
+                      <div class="text-overline">
+                      #{{pokemon.id}} {{pokemon.name}}
+                      </div>
+                      
+                      <div>
+                        HP
+                        <v-progress-linear color="red lighten-1" height="15" :value="pokemon.stats[0]"> <strong>{{pokemon.stats[0]}}</strong> </v-progress-linear>
+                        Attack
+                        <v-progress-linear color="deep-orange lighten-3" height="15" :value="pokemon.stats[1]"><strong>{{pokemon.stats[1]}}</strong></v-progress-linear>
+                        Defense
+                        <v-progress-linear height="15" :value="pokemon.stats[2]" color="light-blue"><strong>{{pokemon.stats[2]}}</strong></v-progress-linear>
+                        Speed
+                        <v-progress-linear :value="pokemon.stats[5]" height="15" color="light-green lighten-2"><strong>{{pokemon.stats[5]}}</strong></v-progress-linear>
+                      </div>
+                    </v-col>
                   </v-row>
-                    
-                  <div class="text-overline">
-                   #{{pokemon.id}} {{pokemon.name}}
-                  </div>
-                  
-                  <div>
-                    HP
-                    <v-progress-linear color="red lighten-1" height="15" :value="pokemon.stats[0]"> <strong>{{pokemon.stats[0]}}</strong> </v-progress-linear>
-                    Attack
-                    <v-progress-linear color="deep-orange lighten-3" height="15" :value="pokemon.stats[1]"><strong>{{pokemon.stats[1]}}</strong></v-progress-linear>
-                    Defense
-                    <v-progress-linear height="15" :value="pokemon.stats[2]" color="light-blue"><strong>{{pokemon.stats[2]}}</strong></v-progress-linear>
-                    Speed
-                    <v-progress-linear :value="pokemon.stats[5]" height="15" color="light-green lighten-2"><strong>{{pokemon.stats[5]}}</strong></v-progress-linear>
-                  </div>
-                </v-col>
-              </v-row>
-            </v-sheet>
-          </v-carousel-item>
-        </v-carousel>
-      </div>
+                </v-sheet>
+              </v-carousel-item>
+            </v-carousel>
+          </div>
+        </v-col>
+      </v-row>
     </v-parallax>
+
   </div>
   
 </template>
@@ -123,7 +140,6 @@ export default {
 </script>
 
 <style scoped>
-
 @import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@100;300&display=swap');
 
 .intro-img{
@@ -141,6 +157,16 @@ export default {
   transform: none !important;
 }
 
+.parallax-title{
+  font-size: 2em;
+  font-family: 'Montserrat', sans-serif;
+}
+
+.parallax-text{
+  font-size: 1.4em;
+  font-family: 'Montserrat', sans-serif;
+}
+
 .intro-text {
   font-size: 2em;
   font-family: 'Montserrat', sans-serif;
@@ -155,9 +181,6 @@ export default {
   text-align: center;
 }
 
-.pokedex-parallax{
-  width: 50vw;
-}
 
 .icon-banner{
   width: 2.4em;
@@ -168,9 +191,13 @@ h4#subtitle.overline.subtitle{
   font-size: 1em !important
 }
 
+
 @media screen and (max-width: 600px) {
-        .intro-text{
-            font-size: 1.2em;
+        .intro-text, .parallax-title{
+          font-size: 1.2em;
+        }
+        .parallax-text{
+          font-size: 1em;
         }
         .intro-img{
           height: 45em;
